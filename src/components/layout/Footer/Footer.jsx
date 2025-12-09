5f6473992867c098202d66276069d5c25448620d
hwang-inah <tkdlekakdrh@gmail.com>
2025-12-09 20:07:46 +0900

    footer css 수정

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -21,7 +21,8 @@ const Footer = () => {
           <div className={styles.companyInfo}>
             <p><strong>프렌즈 프리미엄 골프연습장 신불당점</strong></p>
             <p><span className={styles.miniInfo}>주소</span> 충남 천안시 서북구 불당33길 34 엠타워 5층</p>
-            <p><span className={styles.miniInfo}>사업자번호</span> 482-25-01771 <span className={styles.miniInfo}>이메일</span> abbyyourside@naver.com</p>
+            <p><span className={styles.miniInfo}>사업자번호</span> 482-25-01771</p>
+            <p><span className={styles.miniInfo}>이메일</span> abbyyourside@naver.com</p>
             <p><span className={styles.miniInfo}>상담·문의</span> 010-2426-7366</p>
           </div>
         </div>
